7d00facb17ae4e5ade21f7395246841b8adb3550
juroga_new <juanjrgast@gmail.com>
2023-06-25 19:49:37 +0200

    Created a form to add an event and their views

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,28 @@ class EventsController < ApplicationController
     def index
         @events = Event.all
     end
+
+    def show
+        @event = Event.find(params[:id])
+    end
+
+    def new
+        @event = current_user.created_events.new
+    end
+
+    def create
+        @event = current_user.created_events.build(event_params)
+    
+        if @event.save
+          redirect_to @event, notice: 'Event was successfully created.'
+        else
+          render :new
+        end
+    end
+
+    private
+
+  def event_params
+    params.require(:event).permit(:title, :date, :location)
+  end
 end
