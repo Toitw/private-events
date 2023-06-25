5f81f580f11adf54f780d8ad86dde6f03bd2eac6
juroga_new <juanjrgast@gmail.com>
2023-06-25 20:28:58 +0200

    Attendances controller done

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :events, only: [:index, :show, :new, :create]
+    resources :attendances, only: [:create]
+
+  resources :attendances, only: [:destroy]
   # Defines the root path route ("/")
   # root "articles#index"
   root "events#index"
