419a6c4cd1b77ff118c372ddae893365a8147fb2
juroga_new <juanjrgast@gmail.com>
2023-06-25 13:01:30 +0200

    Add a simple view for index and configure route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :events
   # Defines the root path route ("/")
   # root "articles#index"
+  root "events#index"
 end
