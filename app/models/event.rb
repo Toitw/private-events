1d5e3a5e6d20a0d784795e4144676ec0c5217fb5
juroga_new <juanjrgast@gmail.com>
2023-07-01 13:05:54 +0200

    Added the event titles a link to its event show page and added the list of attendees to each event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
     belongs_to :creator, class_name: "User", :foreign_key => :creator_id
-    has_many :attendances
+    has_many :attendances, foreign_key: :attended_event_id
     has_many :attendees, through: :attendances, source: :attendee
     # scope :upcoming, -> { where("date >= ?", Date.today) }
     # scope :past, -> { where("date < ?", Date.today) }
