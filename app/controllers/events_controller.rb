901014b646878ebef128d85dff1c83795c97eb15
juroga_new <juanjrgast@gmail.com>
2023-07-02 15:19:05 +0200

    Added cancel event option for creators and added past and upcoming sections to the event index

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -22,6 +22,12 @@ class EventsController < ApplicationController
         end
     end    
 
+    def destroy
+        @event = Event.find(params[:id])
+        @event.destroy
+        redirect_to events_path
+    end
+
     private
 
   def event_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :events, only: [:index, :show, :new, :create] do
+  resources :events, only: [:index, :show, :new, :create, :destroy] do
     resources :attendances, only: [:create, :destroy]
   end
   # Defines the root path route ("/")
